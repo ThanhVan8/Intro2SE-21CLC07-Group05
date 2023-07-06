57546b48c084c5c0a0e5ff6ef6c78fca4a86bd0b
Thanh Van <tttvan21@clc.fitus.edu.vn>
2023-07-06 19:37:30 +0700

    setup

---
+++ src/awn-anw/src/App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>Buyer</div>
+  )
+}
+
+export default App
---
+++ src/awn-anw/src/config/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB2PTCMXQ82guBSCSB3dVB_7Kcm9pSsF2c",
+  authDomain: "awn-anw-7ad59.firebaseapp.com",
+  databaseURL: "https://awn-anw-7ad59-default-rtdb.firebaseio.com",
+  projectId: "awn-anw-7ad59",
+  storageBucket: "awn-anw-7ad59.appspot.com",
+  messagingSenderId: "140582882046",
+  appId: "1:140582882046:web:82cc312df065ada031b3af"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
