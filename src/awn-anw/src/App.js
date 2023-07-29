b395770b565b7d816230c3d0d41eaf653c53ec44
Thanh Van <tttvan21@clc.fitus.edu.vn>
2023-07-29 14:35:29 +0700

    signin

--- src/awn-anw/src/App.js
+++ src/awn-anw/src/App.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import {Route, Routes} from 'react-router-dom'
+
+import Signin from './pages/Signin'
+import Signup from './pages/Signup'
+// import ProtectedRoute from './routers/ProtectedRoute'
 
 const App = () => {
   return (
-    <div>Buyer</div>
+    <div className="w-screen h-auto flex flex-col">
+      <Routes>
+        <Route path="/" element={<Signin />} />
+        <Route path="/Signup" element={<Signup />} />
+      </Routes>
+    </div>
   )
 }
 
---
+++ src/awn-anw/src/pages/Signin.jsx
@@ -0,0 +1,42 @@
+import React, {useState} from "react";
+import { Link } from "react-router-dom";
+import Header0 from '../components/Header/Header0'
+import mainpic from '../assets/mainpic.png'
+
+const Signin = () => {
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    return (
+        <>
+            <Header0 />
+            <div className="grid grid-cols-1 md:grid-cols-2 h-screen w-full">
+                <div className="flex flex-col justify-center px-5">
+                    <form className="w-4/5 mx-auto px-10 py-10 flex flex-col items-center rounded-3xl shadow-xl">
+                        <h2 className="text-2xl text-center font-semibold py-1">Sign in</h2>
+                        <div className="flex flex-col py-2 w-full">
+                            <label htmlFor="email">Email</label>
+                            <input className="border border-[#D9D9D9] p-2 rounded-[10px]" type="email" required 
+                                    value={email} onChange={e => setEmail(e.target.value)}/>
+                        </div>
+                        <div className="flex flex-col py-2 w-full">
+                            <label htmlFor="password">Password</label>
+                            <input className="border border-[#D9D9D9] p-2 rounded-[10px]" type="password" required 
+                                    value={password} onChange={e => setPassword(e.target.value)}/>
+                        </div>
+                        <button className="w-28 px-7 py-2.5 my-5 bg-primary text-white rounded-3xl hover:opacity-75">Sign in</button>
+                        <p>Don't have an account? <Link className="text-primary underline hover:opacity-75" to="/Signup">Sign up</Link></p>
+                    </form>
+                </div>
+
+                <div className="hidden md:flex justify-end items-center">
+                    <img src={mainpic} alt="mainpic" className="h-3/4 object-contain fixed" />
+                </div>
+
+            </div>
+        </>
+    )
+}
+
+export default Signin;
