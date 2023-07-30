0fc064261cb384c626d5710eb8deac44dc6c9d7a
Hoang Kim <vonguyenhoangkim14022003@gmail.com>
2023-07-30 14:47:39 +0700

    push to pull again

---
+++ src/awn-anw/src/components/Footer.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import logo from '../../src/assets/logo.png';
+import {Link} from "react-router-dom";
+import { FaFacebook } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+
+
+const Footer = () => {
+  return (
+    <footer className='fixed w-screen bg-primary py-3 pr-8 pl-2 bottom-0'>
+        <div className='hidden md:flex w-full h-full'>
+            <Link to = {"/"} className = 'flex items-center'>
+                <img src={logo} className = 'w-14 object-cover' alt="Logo" />
+            </Link>
+
+            {/* about us */}
+            <ul className='flex items-center mr-auto pl-4'>
+                <li className='text-xl text-textHeadingColor hover:text-textHover cursor-pointer'>About us</li>
+            </ul>
+
+            {/* social media */}
+            <Link to ={"/"} className= "relative flex justify-center gap-4 items-end" >
+                <FaInstagram className=' text-2xl text-textHeadingColor hover:text-textHover cursor-pointer'/>
+                <FaFacebook className=' text-2xl text-textHeadingColor  hover:text-textHover cursor-pointer'/>
+            </Link>
+
+        </div>
+        
+    </footer>
+  )
+}
+
+export default Footer
