c1844c14e16590d36481233358d37c3eb8507946
PhuocTaii <tai.ngo1773@gmail.com>
2023-08-03 22:17:13 +0700

    abc

--- src/awn-anw/src/App.js
+++ src/awn-anw/src/App.js
@@ -1,65 +1,12 @@
 import React from 'react'
-<<<<<<< HEAD
-import { Header } from './components'
-
-const App = () => {
-  return (
-    <div classname = "w-screen h-auto flex flex-col" >
-      <Header />
-=======
 import {Route, Routes} from 'react-router-dom'
 
 import Signin from './pages/Signin'
 import Signup from './pages/Signup'
 import HomeTest from './pages/HomeTest'
 import ProtectedRoute from './routers/ProtectedRoute'
-import { firestore } from './config/firebase'
-import { collection, getDocs } from 'firebase/firestore'
 
 const App = () => {
-  const [categoryList, setCategoryList] = useState([])
-  const CategoryCollectionRef = collection( firestore, "Category")
-
-  const [merchantDetails, setMerchantDetails] = useState([])
-  const MerchantCollectionRef = collection (firestore, "Merchant")
-
-  useEffect(() => {
-    const getCategoryList = async () => {
-      try{
-        const data = await getDocs(CategoryCollectionRef)
-        const filteredData = data.docs.map((doc) => ({
-          ...doc.data(),
-          id: doc.id
-        }));
-        // setCategoryList(filteredData)
-        console.log(filteredData);
-      } catch (err){
-        console.error(err);
-      }
-    };
-
-    getCategoryList();
-  }, [])
-
-  useEffect(() => {
-    const getMerchantDetails = async () => {
-      try{
-        const data = await getDocs(MerchantCollectionRef)
-        const filteredData = data.docs.map((doc) => ({
-          ...doc.data(),
-          id: doc.id
-        }));
-        // setCategoryList(filteredData)
-        console.log(filteredData);
-      } catch (err){
-        console.error(err);
-      }
-    };
-
-    getMerchantDetails();
-  }, [])
-
-
   return (
     <div className="w-screen h-auto flex flex-col">
       <Routes>
