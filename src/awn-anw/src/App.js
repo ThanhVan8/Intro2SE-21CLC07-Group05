af473838b0a6f7080c424490ff84bee07790e03c
Hoang Kim <vonguyenhoangkim14022003@gmail.com>
2023-08-13 09:27:50 +0700

    css for menu page

--- src/awn-anw/src/App.js
+++ src/awn-anw/src/App.js
@@ -5,6 +5,7 @@ import Signin from './pages/Signin'
 import Signup from './pages/Signup'
 import Home from './pages/Home'
 import Cart from './components/Cart'
+import Menu from './pages/Menu'
 import ProtectedRoute from './routers/ProtectedRoute'
 
 const App = () => {
@@ -17,6 +18,8 @@ const App = () => {
         <Route path="/Signup" element={<Signup />} />   
         <Route path='/Home' element = {<Home />}/>     
         <Route path='/Cart' element = {<Cart />}/>     
+        <Route path='/Menu' element = {<Menu />}/>     
+
 
       </Routes>
     </div>
---
+++ src/awn-anw/src/pages/Menu.jsx
@@ -0,0 +1,104 @@
+import React from 'react'
+import Header from '../components/Header/Header'
+import Footer from '../components/Footer'
+import menupic from "../assets/menupic.png";
+import cake from "../assets/cake.jpg";
+
+
+
+
+const Menu = () => {
+  return (
+    <>
+      <Header />
+      <div className='ml-4 mt-20 mb-8'>
+        <div className='grid grid-cols-3 md:grid-cols-3 w-full h-fit gap-4'>
+          
+          <div className='grid col-span-2 grid-cols-1 gap-4 justify-item-start p-12 min-h-420'>
+            <div className=' w-656 border border-black h-150 rounded-2xl'>
+              <div className='grid grid-cols-4 w-full h-full gap-2'>
+                <div>
+                  <img src={cake} alt="Food image" className='h-32 w-32 object-cover rounded-full my-2 mx-4 ' />
+                </div>
+                <div className='grid col-span-2 grid-cols-1 gap-2 justify-items-start my-4 h-fit py-4'>
+                  <div className='text-xl font-semibold text-textColor '>Cheese Cake</div>
+                  <div className='text-base text-textColor opacity-50'>Sweet</div>
+                </div>
+                <div className='relative'>
+                  <button 
+                    className='rounded-3xl border bg-primary border-primary w-16 h-8 my-2 text-textHeadingColor text-base
+                              absolute bottom-2 right-4 hover:opacity-80'>
+                    
+                    ADD
+                  </button>
+                </div>
+                
+              </div>
+            </div>
+
+            <div className=' w-656 border border-black h-150 rounded-2xl'>
+              <div className='grid grid-cols-4 w-full h-full gap-2'>
+                <div>
+                  <img src={cake} alt="Food image" className='h-32 w-32 object-cover rounded-full my-2 mx-4 ' />
+                </div>
+                <div className='grid col-span-2 grid-cols-1 gap-2 justify-items-start my-4 h-fit py-4'>
+                  <div className='text-xl font-semibold text-textColor '>Cheese Cake</div>
+                  <div className='text-base text-textColor opacity-50'>Sweet</div>
+                </div>
+                <div className='relative'>
+                  <button 
+                    className='rounded-3xl border bg-primary border-primary w-16 h-8 my-2 text-textHeadingColor text-base
+                              absolute bottom-2 right-4 hover:opacity-80'>
+                    
+                    ADD
+                  </button>
+                </div>
+                
+              </div>
+            </div>
+
+            <div className=' w-656 border border-black h-150 rounded-2xl'>
+              <div className='grid grid-cols-4 w-full h-full gap-2'>
+                <div>
+                  <img src={cake} alt="Food image" className='h-32 w-32 object-cover rounded-full my-2 mx-4 ' />
+                </div>
+                <div className='grid col-span-2 grid-cols-1 gap-2 justify-items-start my-4 h-fit py-4'>
+                  <div className='text-xl font-semibold text-textColor '>Cheese Cake</div>
+                  <div className='text-base text-textColor opacity-50'>Sweet</div>
+                </div>
+                <div className='relative'>
+                  <button 
+                    className='rounded-3xl border bg-primary border-primary w-16 h-8 my-2 text-textHeadingColor text-base
+                              absolute bottom-2 right-4 hover:opacity-80'>
+                    
+                    ADD
+                  </button>
+                </div>
+                
+              </div>
+            </div>
+            
+          </div>
+          <div className="flex flex-col justify-center items-center mb-8 sticky">
+            <div className='sticky'>
+              <img
+                  src={menupic}
+                  alt="menupic"
+                  className="h-full object-contain pl-5"
+                />
+              <p className='text-xl font-bold text-center pl-24'>Shop name</p>
+
+            </div>
+              
+
+          </div>
+        </div>
+
+      </div>
+      <Footer />
+
+    </>
+  )
+}
+
+export default Menu
