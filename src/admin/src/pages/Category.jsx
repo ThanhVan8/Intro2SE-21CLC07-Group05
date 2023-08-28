871081ec1ee2aabe0880eb98bddddcc7f811dc7d
Hoang Kim <vonguyenhoangkim14022003@gmail.com>
2023-08-28 15:57:50 +0700

    add hover

--- src/admin/src/pages/Category.jsx
+++ src/admin/src/pages/Category.jsx
@@ -9,42 +9,28 @@ const Category = () => {
         <Header/>
            {/*Add category btn */}
             <div className='w-full mt-20 px-16 grid place-content-end'>
-                <button className='bg-primary text-textHeadingColor h-8 px-4 rounded-lg text-base'>Add Category</button>
+                <button className='bg-primary text-textHeadingColor h-8 px-4 rounded-lg text-base hover:bg-opacity-80'>
+                    Add Category
+                </button>
             </div> 
             {/* Category list */}
             <div className='flex flex-col w-full min-h-screen gap-2 py-12 px-16'>
                 <div className='w-full h-fit bg-gray grid grid-cols-2 justify-items-start py-2 px-4 gap-4'>
-                    <div className='text-base'>Nguyen Van A</div>
+                    <div className='text-base'>Fried Chicken</div>
                     <div className='text-base justify-self-end '>
-                        <button className='bg-red h-8 w-8 rounded-full grid place-content-center'>
+                        <button className='bg-red h-8 w-8 rounded-full grid place-content-center hover:bg-opacity-80'>
                             <FaTrashAlt className=' text-textHeadingColor'/>
                         </button>
                     </div>
                 </div>
                 <div className='w-full h-fit bg-gray grid grid-cols-2 justify-items-start py-2 px-4 gap-4'>
-                    <div className='text-base'>Nguyen Van A</div>
+                    <div className='text-base'>Milk tea</div>
                     <div className='text-base justify-self-end '>
-                        <button className='bg-red h-8 w-8 rounded-full grid place-content-center'>
+                        <button className='bg-red h-8 w-8 rounded-full grid place-content-center hover:bg-opacity-80'>
                             <FaTrashAlt className=' text-textHeadingColor'/>
                         </button>
                     </div>
-                </div> 
-                <div className='w-full h-fit bg-gray grid grid-cols-2 justify-items-start py-2 px-4 gap-4'>
-                    <div className='text-base'>Nguyen Van A</div>
-                    <div className='text-base justify-self-end '>
-                        <button className='bg-red h-8 w-8 rounded-full grid place-content-center'>
-                            <FaTrashAlt className=' text-textHeadingColor'/>
-                        </button>
-                    </div>
-                </div> 
-                <div className='w-full h-fit bg-gray grid grid-cols-2 justify-items-start py-2 px-4 gap-4'>
-                    <div className='text-base'>Nguyen Van A</div>
-                    <div className='text-base justify-self-end '>
-                        <button className='bg-red h-8 w-8 rounded-full grid place-content-center'>
-                            <FaTrashAlt className=' text-textHeadingColor'/>
-                        </button>
-                    </div>
-                </div> 
+                </div>
             </div>
             
         <Footer/>
