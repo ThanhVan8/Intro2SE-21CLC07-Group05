6f56e19afe0d8dc1f179fdeae113a6c8279260d5
PhuocTaii <tai.ngo1773@gmail.com>
2023-09-02 15:34:26 +0700

    bug

--- src/merchant/src/components/Dropdown.jsx
+++ src/merchant/src/components/Dropdown.jsx
@@ -77,19 +77,15 @@ const Dropdown = ({isDisable, selectedValue}) => {
   return (
     <select
       value={selectedCategory}
-      disabled={isDisable}
-      autoFocus={!isDisable}
-      className={
-        isDisable
-          ? "w-64 p-2.5 text-gray-500 bg-gray-100 border rounded-md shadow-sm outline-none appearance-none"
-          : "w-64 p-2.5 text-textColor bg-white border rounded-md shadow-sm outline-none appearance-none focus:border-primary hover:border-primary"
-      }
+      autoFocus={true}
+      className={"w-64 p-2.5 text-textColor bg-white border rounded-md shadow-sm outline-none appearance-none focus:border-primary hover:border-primary"}
       onChange={handleSelect}
     >
-      <option>Rice</option>
-      <option>Milk tea</option>
-      <option>Noodle</option>
-      <option>Chicken</option>
+			{category.map(cate => {
+				return (
+					<option>{cate}</option>
+				)
+			})}
     </select>
   );
 };
