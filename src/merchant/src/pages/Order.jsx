7c21ece8917722026d2c3b3e3376e33aab9fa718
PhuocTaii <tai.ngo1773@gmail.com>
2023-09-02 14:17:18 +0700

    merchant order

--- src/merchant/src/pages/Order.jsx
+++ src/merchant/src/pages/Order.jsx
@@ -4,40 +4,54 @@ import Footer from '../components/Footer'
 import { getAuth } from 'firebase/auth'
 import { firestore } from '../config/firebase'
 import useAuth from '../custom_hooks/useAuth'
-import { collection, getDoc, getDocs, query, where, doc } from 'firebase/firestore'
+import { collection, getDoc, getDocs, query, where, doc, or } from 'firebase/firestore'
 import OrderCard from '../components/OrderCard';
 
 const Order = () => {
 
     const [OrderDetail, setOrder] = useState([])
     const [BuyerDetail, setBuyer] = useState([])
+    const [Food, setFood] = useState([])
     const merchant = useAuth();
-    let u_id = []
     const getOrder = async(uid) => {
         try{
           const OrderRef = collection(firestore, "Order")
+          const FoodRef = doc(firestore, "Menu", uid)
+          let order = []
+          let buyer = []
+          let food = []
+          let u_id = []
+          let foodList = []
           const q = query(OrderRef, where ("M_ID", "==", uid));
           const querySnapshot = await getDocs(q)
           querySnapshot.forEach((doc) => {
-            const OrderData = {...doc.data(), id: doc.id};
-            // console.log(OrderData);
-            // setOrder(OrderData);
-            setOrder([
-              ...OrderDetail,
-              OrderData
-            ]);
+            const OrderData = doc.data();
+            order.push(OrderData);
             u_id.push(OrderData.O_ID);
+            food.push(OrderData.Food);
           })
+          setOrder(order); // So luong cua tung mon an
+
+          for(let i = 0; i < food.length; i++){
+            let food_list = []
+            for(let j = 0; j < food[i].length; j++){
+              const docSnap = await getDoc(FoodRef)
+              const FoodData = docSnap.data()['FoodList']
+              food_list.push(FoodData[food[i][j]])
+            }
+            foodList.push(food_list)
+            
+          }
+          // console.log(foodList);
+          setFood(foodList); // Ten cua tung mon an
+
           for (let i = 0; i < u_id.length; i++){
             const BuyerRef = doc(firestore, "User", u_id[i])
             const docSnap = await getDoc(BuyerRef)
-            const buyer = docSnap.data();
-            setBuyer([
-                ...BuyerDetail,
-                buyer // need to check here
-            ]);
-            // console.log(buyer);
+            const Buyer = docSnap.data();
+            buyer.push(Buyer);
           }
+          setBuyer(buyer); //Thong tin khach hang
         } catch(err){
           console.error(err)
         }
