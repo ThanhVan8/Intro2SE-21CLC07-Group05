0a108c4dedb8487019f5239c3f054bbaa7b0e4fd
Thanh Van <tttvan21@clc.fitus.edu.vn>
2023-09-04 15:13:39 +0700

    delete category

--- src/merchant/src/pages/Home.jsx
+++ src/merchant/src/pages/Home.jsx
@@ -43,7 +43,7 @@ const Home = () => {
         {/* DISPLAY STORE INFORMATION */}
         <div className="grid grid-cols-2 w-full pt-16 min-h-screen font-mono text-base">
           <div key={OneMerchant.uid} className='grid text-textColor text-left h-2/3 justify-items-start py-28 px-12 my-24 mx-10 gap-y-2 '>
-            <p className=' h-fit text-3xl font-semibold mb-4'>
+            <p className=' h-fit text-3xl font-semibold mb-4 font-mono'>
              {OneMerchant.Name}
             </p>
 
